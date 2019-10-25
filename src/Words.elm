095d6b795e7747db5c01173a3b6096c3aa9af722
Dave Rapin <dave@rapin.com>
2019-10-25 10:47:39 -0400

    Fixed amazing dad

--- src/Words.elm
+++ src/Words.elm
@@ -23,7 +23,7 @@ words =
     , ( "Matatata", "Hi" )
     , ( "Tamamama", "High" )
     , ( "Toff", "Mom" )
-    , ( "Kell", "Amazing Dad" )
+    , ( "Kell", "Dad" )
     , ( "Swop", "Play" )
     , ( "Pel", "Nolan" )
     , ( "Cep", "Zion" )
